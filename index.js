8ad087fbfe33598b0474f1726909c5db8e8feee9
Siyamthanda Tsobo <siyamthandatsobo@gmail.com>
2023-11-20 13:47:00 +0200

    finished functions task

---
+++ index.js
@@ -0,0 +1,16 @@
+function greet(name){
+    console.log('Hello ' + name)
+    }
+    
+greet('siyamthanda')
+function multiply(a,b){
+    console.log(a*b)
+}
+multiply(2,3)
+function tableOfFive(a){
+    result=a*5
+    console.log(result)
+    return result
+
+}
+tableOfFive(5)
